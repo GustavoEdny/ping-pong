3ac39963d9c6dcece438750d2938ea15445113d0
Gustavo Edny <gustavoedny@gmail.com>
2020-04-13 12:32:23 -0300

    adding tests to reply method

--- test/ping_pong_test.exs
+++ test/ping_pong_test.exs
@@ -2,7 +2,13 @@ defmodule PingPongTest do
   use ExUnit.Case
   doctest PingPong
 
-  test "greets the world" do
-    assert PingPong.hello() == :world
+  test "reply returns :pong when receive :ping" do
+    assert PingPong.reply(:ping) == :pong
+  end
+
+  test "reply returns :ping when receive any value" do
+    assert PingPong.reply(:pong) == :ping
+    assert PingPong.reply("pong") == :ping
+    assert PingPong.reply([]) == :ping
   end
 end
