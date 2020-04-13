c22a2adc5a7d8a3cf67e76e20dda8f5b57030f1d
Gustavo Edny <gustavoedny@gmail.com>
2020-04-13 12:19:29 -0300

    Methods to send messages between process created

--- lib/ping_pong.ex
+++ lib/ping_pong.ex
@@ -1,18 +1,42 @@
 defmodule PingPong do
-  @moduledoc """
-  Documentation for `PingPong`.
-  """
+  @count_limit 10
 
-  @doc """
-  Hello world.
+  def main do
+    lisa = self()
+    bob = PingPong |> Task.start(:listen, []) |> elem(1)
 
-  ## Examples
+    IO.puts "lisa #{inspect lisa}"
+    IO.puts "lisa #{inspect bob}"
 
-      iex> PingPong.hello()
-      :world
-
-  """
-  def ready do
-    :world
+    send_to(bob, :ping, 1)
+    listen()
   end
+
+  def listen do
+    receive do
+      {_sender, _message, @count_limit} ->
+        IO.puts "Call finished."
+        listen()
+
+      {sender, message, count} ->
+        send_to(sender, reply(message), count + 1)
+        listen()
+    after
+      1_000 -> IO.puts "Timing out call #{inspect process_pid()}"
+    end
+  end
+
+  def send_to(process_id, action, count) do
+    IO.puts "Called #{count} times. send_to #{inspect process_id} #{inspect action}"
+    send(process_id, {process_pid(), action, count})
+  end
+
+  def reply(message) do
+    case message do
+      :ping -> :pong
+      _____ -> :ping
+    end
+  end
+
+  defp process_pid, do: self()
 end
